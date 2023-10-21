f502720d78e8c2582fac13f7f74e9724036cb677
rajpreetcodes <143878990+rajpreetcodes@users.noreply.github.com>
2023-10-21 15:58:39 +0530

    Create 8_q4.cpp

---
+++ C++/Exp_08/8_q4.cpp
@@ -0,0 +1,22 @@
+//WAP to print array using pointer
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    int a[100],n,i;
+    cout<<"How many numbers:";
+    cin>>n;
+    cout<<"Enter the numbers"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"array elements using pointer:";
+    for(i=0;i<n;i++)
+    {
+        //cout<<a[i]<<endl;
+        cout<<*(p+i)<<endl;
+    }
+    return 0;
+}
